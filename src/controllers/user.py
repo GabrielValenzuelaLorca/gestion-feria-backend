4966384eb7a1f78472364b680e5fa028e42c537d
Gabriel Valenzuela <gabriel.valenzuela.lorca@gmail.com>
2021-12-06 00:30:34 -0300

    Add login endpoint and behavior

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,11 +1,12 @@
 from bson.objectid import ObjectId
 from app import get_db, encode_auth_token
 from flask import request
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_api import status
 
 def registerService():
   db = get_db()
-  
+
   user = {
     "email": request.json["email"],
     "name": request.json["name"],
@@ -18,3 +19,24 @@ def registerService():
     "name": user["name"]
   }
   return res
+
+def loginService():
+  db = get_db()
+
+  user = db.user.find_one({
+    "email": request.json["email"]
+  })
+
+  if user is None:
+    return "Usuario no encontrado", status.HTTP_404_NOT_FOUND
+
+  elif not check_password_hash(user["password"], request.json["password"]):  
+    return "Contraseña incorrecta", status.HTTP_401_UNAUTHORIZED
+    
+  else:
+    res = {
+      "auth_token": encode_auth_token(str(ObjectId(user["_id"]))),
+      "email": user["email"],
+      "name": user["name"]
+    }
+    return res
--- src/routes/user.py
+++ src/routes/user.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
-from controllers.user import registerService
+from controllers.user import loginService, registerService
 
 userRoute = Blueprint('user', __name__, url_prefix='/user')
 
 @userRoute.route('/register', methods=['POST'])
 def register():
   return registerService()
+
+@userRoute.route('/login', methods=['POST'])
+def login():
+  return loginService()
