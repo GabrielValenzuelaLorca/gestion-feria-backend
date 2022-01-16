fb3873a70f33ebf638c2fb82fe2824635b2edc00
Gabriel Valenzuela <gabriel.valenzuela.lorca@gmail.com>
2022-01-15 23:02:55 -0300

    Add token authorization check

--- src/app.py
+++ src/app.py
@@ -42,7 +42,7 @@ def encode_auth_token(user_id):
     
 def decode_auth_token(auth_token):
   try:
-    payload = jwt.decode(auth_token, current_app.config.get('SECRET_KEY'))
+    payload = jwt.decode(auth_token, current_app.config.get('SECRET_KEY'), algorithms="HS256")
     return payload['sub']
   except jwt.ExpiredSignatureError:
     return 'Signature expired. Please log in again.'
---
+++ src/wrappers.py
@@ -0,0 +1,25 @@
+from functools import wraps
+from flask import request
+from app import decode_auth_token, get_db
+from bson.objectid import ObjectId
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].replace('Bearer ', '')
+        if not token:
+            return 'Unauthorized Access!', 401
+
+        try:
+            db = get_db()
+            data = decode_auth_token(token)
+            current_user = db.user.find_one({'_id': ObjectId(data)})
+            if not current_user:
+                return 'Unauthorized Access!', 401
+        except:
+            return 'Unauthorized Access!', 401
+        return f(*args, **kwargs)
+
+    return decorated
